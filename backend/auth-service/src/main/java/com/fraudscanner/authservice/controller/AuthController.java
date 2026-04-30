87f7f06a5770a6245bb07ca9909a40b1f91d1a25
marwenjammazi171 <https://github.com/marwenjammazi.com>
2026-04-30 17:34:28 +0100

    first commit

---
+++ backend/auth-service/src/main/java/com/fraudscanner/authservice/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.fraudscanner.authservice.controller;
+
+import com.fraudscanner.authservice.dto.AuthResponse;
+import com.fraudscanner.authservice.dto.LoginRequest;
+import com.fraudscanner.authservice.dto.RegisterRequest;
+import com.fraudscanner.authservice.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+private final AuthService authService ;
+public AuthController (AuthService authService){
+    this.authService=authService ;
+}
+@PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request){
+AuthResponse response = authService.register(request);
+return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+}
+@PostMapping("/login")
+public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+    AuthResponse response = authService.login(request);
+    return ResponseEntity.ok(response);
+}
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Auth Service is running");
+    }
+}
+
