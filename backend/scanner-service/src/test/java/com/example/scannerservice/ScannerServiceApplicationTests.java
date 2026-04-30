8a274700bf4dd56554d12972bab008b1df50d12d
marwenjammazi171 <https://github.com/marwenjammazi.com>
2026-04-30 18:43:58 +0100

    creating the scanner service module ps to maven

---
+++ backend/scanner-service/src/test/java/com/example/scannerservice/ScannerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scannerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScannerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
