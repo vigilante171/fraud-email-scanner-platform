984d1e8388568de89d044811b782ecc0097ef0d6
marwenjammazi171 <https://github.com/marwenjammazi.com>
2026-04-30 17:58:03 +0100

    creating the entity of the audit log

--- backend/audit-service/src/main/java/com/fraudscanner/auditservice/entity/AuditLog.java
+++ backend/audit-service/src/main/java/com/fraudscanner/auditservice/entity/AuditLog.java
@@ -1,4 +1,49 @@
 package com.fraudscanner.auditservice.entity;
 
+import com.fraudscanner.auditservice.enums.AuditEventType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "audit_logs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuditLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long emailId;
+
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AuditEventType eventType;
+
+    @Column(nullable = false)
+    private String performedBy;
+
+    @Column(columnDefinition = "TEXT")
+    private String details;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now();
+        }
+
+        if (performedBy == null || performedBy.isBlank()) {
+            performedBy = "system";
+        }
+    }
 }
