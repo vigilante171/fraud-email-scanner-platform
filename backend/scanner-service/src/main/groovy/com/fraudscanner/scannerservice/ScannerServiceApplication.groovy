6a5cb501805bd9814bd208b8857b72bdbacd0a16
marwenjammazi171 <https://github.com/marwenjammazi.com>
2026-04-30 18:41:03 +0100

    creating the scanner service module

---
+++ backend/scanner-service/src/main/groovy/com/fraudscanner/scannerservice/ScannerServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.fraudscanner.scannerservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ScannerServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ScannerServiceApplication, args)
+	}
+
+}
